ca10b3f8041ebdf423e1a91cc59e2b12b0ed8ad3
therealalexolthoff <alex@1452design.com>
2024-02-15 15:25:15 -0500

    Created loader

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let apiQuotes = [];
 const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterButton = document.getElementById('twitter')
+const loader = document.getElementById('loader')
+
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+function loadComplete() {
+    loader.hidden = true
+    quoteContainer.hidden = false
+}
 
 // Add Event Listeners
 document.getElementById('quote-changer').addEventListener('click', getNewQuote)
@@ -19,9 +30,11 @@ function tweet(){
 
 // Show New Quote
 function getNewQuote() {
+  loading()
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   quoteText.innerText = quote.text
   authorText.innerText = quote.author ? quote.author : "Unknown"
+  loadComplete()
 
 //   Check quote length
 quote.text.length > 50 ?  quoteText.classList.add('long-quote'): quoteText.classList.remove('long-quote')
